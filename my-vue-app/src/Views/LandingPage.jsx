2de0c6db81f61b08ec1a701f7cc6ab9d8fa3d1fa
Ema Schönborn <schonborn13@gmail.com>
2023-03-23 03:44:40 -0300

    primer prueba tailwind Landing

--- my-vue-app/src/Views/LandingPage.jsx
+++ my-vue-app/src/Views/LandingPage.jsx
@@ -2,9 +2,15 @@ import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
-      <Link to="/login">Ingresar</Link>
+    <div className="flex flex-col h-screen justify-center items-center bg-gray-100">
+      <h3>Bienvenido a</h3>
+      <h1 className="text-4xl font-bold mb-8">CONECTADOS TECH</h1>
+      <Link
+        class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+        to="/login"
+      >
+        Ingresar
+      </Link>
     </div>
   );
 };
