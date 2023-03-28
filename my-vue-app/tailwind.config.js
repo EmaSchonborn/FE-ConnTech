9d3171bcfc7e61668ecab7efb04e44cb302e0e24
Braian Ruiz <braianruiz14@gmail.com>
2023-03-28 16:29:07 -0300

    nuevo

--- my-vue-app/tailwind.config.js
+++ my-vue-app/tailwind.config.js
@@ -1,16 +1,13 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
-  darkMode: false,
-  theme: {
-    extend: {
-    },
-    fontFamily: {
-      'D-DIN': ['D-DIN', 'sans-serif'],
-    },
-  },
-  variants: {},
-  plugins: [],
-};
-  plugins: [require("daisyui")],
+	mode: "jit",
+	purge: ["./index.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+	darkMode: false,
+	theme: {
+		extend: {},
+		fontFamily: {
+			"D-DIN": ["D-DIN", "sans-serif"],
+		},
+	},
+	variants: {},
+	plugins: [require("daisyui")],
 };
