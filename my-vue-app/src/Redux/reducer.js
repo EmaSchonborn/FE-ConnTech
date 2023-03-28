40491456def1e7f0cbe05a37b114e4b624fcd757
nicoyabichino <nicoyabichino@gmail.com>
2023-03-28 00:51:02 -0300

    prueba3

--- my-vue-app/src/Redux/reducer.js
+++ my-vue-app/src/Redux/reducer.js
@@ -1,12 +1,38 @@
-const intialState = {
-  workers: [],
+import { GET_VACANT, GET_DETAILS, GET_USERS, GET_USER_BY_ID } from "../redux/actions";
+
+const initialState = {
+    vacants: [],
+    vacantDetail: [],
+    empresas : [],
+    users:[],
+    userDetail: []
 };
 
-const rootReducer = (state = intialState, action) => {
-  switch (action.type) {
-    default:
-      return state;
-  }
-};
+const rootReducer = (state=initialState, action) => {
+    switch(action.type){
+        case GET_VACANT:
+            return{ 
+                ...state,
+                vacants:action.payload,
+                }
+        case GET_DETAILS:
+            return{ 
+                ...state,
+                vacantDetail: action.payload
+                }
+        case GET_USERS:
+            return{
+                ...state,
+                users: action.payload
+            }
+        case GET_USER_BY_ID:
+            return{
+                ...state,
+                userDetail: action.payload
+            }            
+        default: 
+        return {...state};
+    }
+}; 
 
 export default rootReducer;
