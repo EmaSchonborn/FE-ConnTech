3be3b75f3572957054c78b67d5e5b1f341a742d0
Braian Ruiz <braianruiz14@gmail.com>
2023-03-29 21:40:01 -0300

    login listo

--- my-vue-app/src/Views/Login.jsx
+++ my-vue-app/src/Views/Login.jsx
@@ -1,31 +1,37 @@
 import { Link } from "react-router-dom";
 
-const Login = () => {
-  return (
-    <div>
-      <div>
-        <Link to="/">Back</Link>
-      </div>
-      <div class="md:flex bg-slate-100">
-        <>
-          <h3>Nombre</h3>
-          <input></input>
-        </>
-        <>
-          <h3>Apellido</h3>
-          <input></input>
-        </>
-        <>
-          <h3>Correo Electronico</h3>
-          <input></input>
-        </>
-        <>
-          <h3>Contraseña</h3>
-          <input></input>
-        </>
-      </div>
-    </div>
-  );
-};
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-export default Login;
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Enviar los valores de correo electrónico y contraseña a través de una llamada a una API de inicio de sesión
+    // Restablecer los valores de correo electrónico y contraseña
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Correo electrónico:
+        <input type="text" value={email} onChange={handleEmailChange} />
+      </label>
+      <label>
+        Contraseña:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <button type="submit">Iniciar sesión</button>
+    </form>
+  );
+}
+
