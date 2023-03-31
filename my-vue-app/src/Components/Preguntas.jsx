cbf51ffe1b3bea21015e6a4df643560293b3b205
unknown <guillermooscarn@gmail.com>
2023-03-31 10:33:20 -0300

    arreglos

---
+++ my-vue-app/src/Components/Preguntas.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Preguntas = () => {
+	return (
+		<div className='w-full h-full bg-gradient-to-r from-sky-500 to-indigo-500 flex flex-col items-center justify-center'>
+			<div class='font-bold'>Preguntas</div>
+			<div>
+				<div>
+					{" "}
+					¿Cómo puedo enviar mi solicitud para un trabajo en
+					particular?
+				</div>
+				<br />
+				<label>
+					{" "}
+					Generalmente, puedes enviar tu solicitud a través del sitio
+					web de la empresa o utilizando la información de contacto
+					proporcionada en la descripción del trabajo. Si tienes
+					alguna duda, comunícate con el departamento de recursos
+					humanos de la empresa.
+				</label>
+				<br />
+				<div> ¿Qué documentos necesito para solicitar un trabajo?</div>
+				<br />
+				<label>
+					{" "}
+					Los documentos que necesites pueden variar según la empresa
+					y el trabajo. Por lo general, necesitarás una copia
+					actualizada de tu currículum y una carta de presentación. Si
+					se requieren otros documentos, la empresa los especificará
+					en la descripción del trabajo.
+				</label>
+				<br />
+				<div>¿Cómo puedo saber si mi solicitud fue recibida?</div>
+				<br />
+				<label>
+					{" "}
+					Si la empresa tiene un sistema de seguimiento de candidatos,
+					deberías recibir una confirmación de que se recibió tu
+					solicitud. De lo contrario, puedes contactar al departamento
+					de recursos humanos de la empresa para confirmar que
+					recibieron tu solicitud.
+				</label>
+				<div>¿Cómo hago para crear una oferta?</div>
+				<br />
+				<label>
+					{" "}
+					Inicia sesión y selecciona el botón “Publicar oferta”. Rellena los campos obligatorios como título y descripción, así como otros que encuentres de utilidad para explicar de forma clara en qué consiste la oferta de trabajo.
+
+				</label>
+				
+			</div>
+		</div>
+	);
+};
+export default Preguntas;
--- my-vue-app/src/Views/Login.jsx
+++ my-vue-app/src/Views/Login.jsx
@@ -1,8 +1,11 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { verifyUser } from "../redux/actions";
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const dispatch=useDispatch();
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -14,24 +17,34 @@ export default function Login() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    dispatch(verifyUser(email,password));
     // Enviar los valores de correo electrónico y contraseña a través de una llamada a una API de inicio de sesión
     // Restablecer los valores de correo electrónico y contraseña
     setEmail('');
     setPassword('');
   };
+  const userVerified=useSelector((state)=>state.userVerified)
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Correo electrónico:
-        <input type="text" value={email} onChange={handleEmailChange} />
-      </label>
-      <label>
-        Contraseña:
-        <input type="password" value={password} onChange={handlePasswordChange} />
-      </label>
-      <button type="submit">Iniciar sesión</button>
-    </form>
+    <div>
+      <div>
+        <Link to="/">Back</Link>
+      </div>
+      <div class="md:flex bg-slate-100">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Correo electrónico:
+            <input type="text" value={email} onChange={handleEmailChange} />
+          </label>
+          <label>
+            Contraseña:
+            <input type="password" value={password} onChange={handlePasswordChange} />
+          </label>
+          <button type="submit">Iniciar sesión</button>
+        </form>
+      </div>
+      {userVerified?console.log(userVerified):null}
+    </div>
   );
 }
 
