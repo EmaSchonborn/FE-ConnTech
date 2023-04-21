ad7ef85d7bc7efecaf59f0020d637e3d289c3095
alearra <alejandroarra.devv@gmail.com>
2023-04-21 19:27:10 -0300

    home - card - profile - buttons - styles

--- my-vue-app/src/components/Card/Card.jsx
+++ my-vue-app/src/components/Card/Card.jsx
@@ -3,23 +3,23 @@ import { Link } from "react-router-dom";
 
 const Card = (props) => {
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 mb-5 bg-slate-300 w-6/12 rounded-md">
       <img
         src="https://thumbs.dreamstime.com/b/marcador-de-foto-avatar-perfil-predeterminado-ilustraci%C3%B3n-vectorial-189495158.jpg"
         alt=""
         className="w-10 h-10"
       />
 
-      {props.name?<h3 className="px-4 py-2 ">{props.name}</h3>:<h3 className="px-4 py-2 ">{props.title}</h3>}
-      {props.phone?<p className="px-4 py-2">Teléfono: {props.phone}</p>:null}
+      {props.name?<h3 className="px-4 py-2 text-black text-base ">{props.name}</h3>:<h3 className="px-4 py-2 ">{props.title}</h3>}
+      {props.phone?<p className="px-4 py-2 text-black text-base">Teléfono: {props.phone}</p>:null}
       <div className="px-4 py-2">
       {/* {props.description?<p className="px-4 py-2">Descripcion: {props.description}</p>:null} */}
         {props.name? <Link className="flex justify-center" to={`/detail/${props.id}`}>
-          <h2 className="flex justify-center bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors duration-300">
+          <h2 className="flex justify-center bg-black  text-base text-white px-4 py-2 rounded-md hover:bg-slate-50 hover:text-black transition-colors duration-300">
             <p>Perfil</p>
           </h2>
         </Link> : <Link className="flex justify-center" to={`/vacant-detail/${props.id}`}>
-          <h2 className="flex justify-center bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors duration-300">
+          <h2 className="flex justify-center bg-black text-base text-slate-50-4 py-2 rounded-md hover:bg-slate-50 hover:text-black transition-colors duration-300">
             <p>Detalle</p>
           </h2>
         </Link>}
